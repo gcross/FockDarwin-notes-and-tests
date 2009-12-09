3d5cfa3377a9a2a1ce6da9e0dbd490bd42fe7000
Gregory Crosswhite <gcross@phys.washington.edu>
2009-12-09 13:38:04 -0800

    Modified code to use the new differential operator testing framework.

--- test-fock.hs
+++ test-fock.hs
@@ -9,10 +9,11 @@ import Control.Applicative.Infix
 
 import Data.Complex
 
-import Quantum.Euclidean3D
-import Quantum.Function
-import Quantum.Function.Transformers
-import Quantum.Testing
+import Data.Differentiable
+import Data.Differentiable.FunctionExpansion
+import Data.Differentiable.Number
+import Data.Differentiable.Quantum3D
+import Data.Differentiable.Testing
 
 import System.IO.Unsafe
 import System.Random
@@ -22,7 +23,6 @@ import Test.Framework
 import Test.Framework.Providers.HUnit
 import Test.Framework.Providers.QuickCheck2
 import Test.QuickCheck
--- @nonl
 -- @-node:gcross.20091204093401.3555:<< Import needed modules >>
 -- @nl
 
@@ -38,24 +38,31 @@ om_z :: Complex Double
 om_z = unsafePerformIO randomIO
 
 -- @-node:gcross.20091204093401.3561:Constants
+-- @+node:gcross.20091209132934.1287:Types
+-- @+node:gcross.20091209132934.1288:QuantumOperator
+type QuantumOperator = DifferentialOperator (Complex Double)
+-- @-node:gcross.20091209132934.1288:QuantumOperator
+-- @-node:gcross.20091209132934.1287:Types
 -- @+node:gcross.20091204093401.3559:Operators
--- @+node:gcross.20091204093401.3560:x, y, z
-x = r_x
-y = r_y
-z = r_z
--- @-node:gcross.20091204093401.3560:x, y, z
--- @+node:gcross.20091207130316.1277:px, py, pz
-px = p_x
-py = p_y
-pz = p_z
--- @-node:gcross.20091207130316.1277:px, py, pz
+-- @+node:gcross.20091209132934.1289:x, y, x
+x, y, z :: QuantumOperator
+x = r_ X
+y = r_ Y
+z = r_ Z
+-- @-node:gcross.20091209132934.1289:x, y, x
+-- @+node:gcross.20091209132934.1290:px, py, px
+px, py, pz :: QuantumOperator
+px = p_ X
+py = p_ Y
+pz = p_ Z
+-- @-node:gcross.20091209132934.1290:px, py, px
 -- @+node:gcross.20091204093401.3592:a, b, c
-a  = sqrt(om/2)  *|x |+| i*sqrt(1/(2*om))  *|p_x
-at = sqrt(om/2)  *|x |-| i*sqrt(1/(2*om))  *|p_x
-b  = sqrt(om/2)  *|y |+| i*sqrt(1/(2*om))  *|p_y
-bt = sqrt(om/2)  *|y |-| i*sqrt(1/(2*om))  *|p_y
-c  = sqrt(om_z/2)*|z |+| i*sqrt(1/(2*om_z))*|p_z
-ct = sqrt(om_z/2)*|z |-| i*sqrt(1/(2*om_z))*|p_z
+a  = sqrt(om/2)  *|x |+| i*sqrt(1/(2*om))  *|px
+at = sqrt(om/2)  *|x |-| i*sqrt(1/(2*om))  *|px
+b  = sqrt(om/2)  *|y |+| i*sqrt(1/(2*om))  *|py
+bt = sqrt(om/2)  *|y |-| i*sqrt(1/(2*om))  *|py
+c  = sqrt(om_z/2)*|z |+| i*sqrt(1/(2*om_z))*|pz
+ct = sqrt(om_z/2)*|z |-| i*sqrt(1/(2*om_z))*|pz
 -- @-node:gcross.20091204093401.3592:a, b, c
 -- @+node:gcross.20091206113753.1374:g, n
 g  = 1/sqrt 2*|(a  |+| i*|b )
@@ -63,22 +70,18 @@ gt = 1/sqrt 2*|(at |-| i*|bt)
 n  = 1/sqrt 2*|(b  |+| i*|a )
 nt = 1/sqrt 2*|(bt |-| i*|at)
 -- @-node:gcross.20091206113753.1374:g, n
--- @+node:gcross.20091206205300.1498:j_x, j_y, j_z
-j_x = y.p_z |-| z.p_y
-j_y = z.p_x |-| x.p_z
-j_z = x.p_y |-| y.p_x
-
-jx = j_x
-jy = j_y
-jz = j_z
--- @-node:gcross.20091206205300.1498:j_x, j_y, j_z
+-- @+node:gcross.20091206205300.1498:jx, jy, jz
+jx = y.pz |-| z.py
+jy = z.px |-| x.pz
+jz = x.py |-| y.px
+-- @-node:gcross.20091206205300.1498:jx, jy, jz
 -- @-node:gcross.20091204093401.3559:Operators
 -- @+node:gcross.20091204093401.3557:Hamiltonians
 -- @+node:gcross.20091204093401.3558:Original
 original_hamiltonian :: QuantumOperator
 original_hamiltonian =
-     1/2 *| (p_x|^2 |+| p_y|^2 |+| p_z|^2)
- |+| bigOmega *| ( x.p_y |-| y.p_x )
+     1/2 *| (px|^2 |+| py|^2 |+| pz|^2)
+ |+| bigOmega *| ( x.py |-| y.px )
  |+| om**2/2 *| ( x|^2 |+| y|^2 )
  |+| om_z**2/2 *| z|^2
 -- @-node:gcross.20091204093401.3558:Original
@@ -104,14 +107,6 @@ makeHOFromCoordinates q p om = 1/2*|p|^2 |+| 1/2*om**2*|q|^2
 -- @+node:gcross.20091204093401.3994:makeHOFromLadders
 makeHOFromLadders a at om = om*|(at.a |+| 0.5*|id)
 -- @-node:gcross.20091204093401.3994:makeHOFromLadders
--- @+node:gcross.20091206205300.1385:=~=
-infix 4 ~=~
-(~=~) :: QuantumOperator -> QuantumOperator ->
-          Function (ThreeDimensions (Complex Double)) XYZ (Complex Double) ->
-          ThreeDimensions (Complex Double) ->
-          Bool
-(~=~) o1 o2 f v = o1 f $> v ~= o2 f $> v
--- @-node:gcross.20091206205300.1385:=~=
 -- @-node:gcross.20091204093401.3991:Functions
 -- @+node:gcross.20091207130316.1295:Miscellaneous expressions
 -- @+node:gcross.20091207130316.1296:t1/t2
@@ -133,15 +128,18 @@ main = defaultMain
             -- @    @+others
             -- @+node:gcross.20091204093401.3988:x coordinate
             [testProperty "x coordinate" $
-                makeHOFromCoordinates x p_x om ~=~ makeHOFromLadders a at om
+                makeHOFromCoordinates x px om =~= makeHOFromLadders a at om
+            -- @nonl
             -- @-node:gcross.20091204093401.3988:x coordinate
             -- @+node:gcross.20091204093401.3990:y coordinate
             ,testProperty "y coordinate" $
-                makeHOFromCoordinates y p_y om ~=~ makeHOFromLadders b bt om
+                makeHOFromCoordinates y py om =~= makeHOFromLadders b bt om
+            -- @nonl
             -- @-node:gcross.20091204093401.3990:y coordinate
             -- @+node:gcross.20091204093401.3996:z coordinate
             ,testProperty "z coordinate" $
-                makeHOFromCoordinates z p_z om_z ~=~ makeHOFromLadders c ct om_z
+                makeHOFromCoordinates z pz om_z =~= makeHOFromLadders c ct om_z
+            -- @nonl
             -- @-node:gcross.20091204093401.3996:z coordinate
             -- @-others
             ]
@@ -150,19 +148,24 @@ main = defaultMain
         ,testGroup "Correct commutator"
             -- @    @+others
             -- @+node:gcross.20091206113753.1377:a
-            [testProperty "a" $ (a ~~ at) ~=~ id
+            [testProperty "a" $ (a ~~ at) =~= id
+            -- @nonl
             -- @-node:gcross.20091206113753.1377:a
             -- @+node:gcross.20091206113753.1379:b
-            ,testProperty "b" $ (b ~~ bt) ~=~ id
+            ,testProperty "b" $ (b ~~ bt) =~= id
+            -- @nonl
             -- @-node:gcross.20091206113753.1379:b
             -- @+node:gcross.20091206113753.1381:c
-            ,testProperty "c" $ (c ~~ ct) ~=~ id
+            ,testProperty "c" $ (c ~~ ct) =~= id
+            -- @nonl
             -- @-node:gcross.20091206113753.1381:c
             -- @+node:gcross.20091206113753.1383:g
-            ,testProperty "g" $ (g ~~ gt) ~=~ id
+            ,testProperty "g" $ (g ~~ gt) =~= id
+            -- @nonl
             -- @-node:gcross.20091206113753.1383:g
             -- @+node:gcross.20091206113753.1385:n
-            ,testProperty "n" $ (n ~~ nt) ~=~ id
+            ,testProperty "n" $ (n ~~ nt) =~= id
+            -- @nonl
             -- @-node:gcross.20091206113753.1385:n
             -- @-others
             ]
@@ -171,27 +174,27 @@ main = defaultMain
         ,testGroup "Correct definition of old in terms of new"
             -- @    @+others
             -- @+node:gcross.20091206205300.1384:x
-            [testProperty "x" $ x ~=~ (1/2/sqrt(om)) *| (gt|+|g |+| i*|(nt|-|n) )
+            [testProperty "x" $ x =~= (1/2/sqrt(om)) *| (gt|+|g |+| i*|(nt|-|n) )
             -- @nonl
             -- @-node:gcross.20091206205300.1384:x
             -- @+node:gcross.20091206205300.1493:y
-            ,testProperty "y" $ y ~=~ (1/2/sqrt(om)) *| (nt|+|n |+| i*|(gt|-|g) )
+            ,testProperty "y" $ y =~= (1/2/sqrt(om)) *| (nt|+|n |+| i*|(gt|-|g) )
             -- @nonl
             -- @-node:gcross.20091206205300.1493:y
             -- @+node:gcross.20091207130316.1292:z
-            ,testProperty "z" $ z ~=~ (1/sqrt(2*om_z)) *| (ct|+|c)
+            ,testProperty "z" $ z =~= (1/sqrt(2*om_z)) *| (ct|+|c)
             -- @nonl
             -- @-node:gcross.20091207130316.1292:z
             -- @+node:gcross.20091206205300.1495:p_x
-            ,testProperty "p_x" $ p_x ~=~ (i*sqrt(om)/2) *| (gt|-|g |+| i*|(nt|+|n) )
+            ,testProperty "px" $ px =~= (i*sqrt(om)/2) *| (gt|-|g |+| i*|(nt|+|n) )
             -- @nonl
             -- @-node:gcross.20091206205300.1495:p_x
             -- @+node:gcross.20091206205300.1497:p_y
-            ,testProperty "p_y" $ p_y ~=~ (i*sqrt(om)/2) *| (nt|-|n |+| i*|(gt|+|g) )
+            ,testProperty "py" $ py =~= (i*sqrt(om)/2) *| (nt|-|n |+| i*|(gt|+|g) )
             -- @nonl
             -- @-node:gcross.20091206205300.1497:p_y
             -- @+node:gcross.20091207130316.1294:p_z
-            ,testProperty "p_z" $ p_z ~=~ (i*sqrt(om_z/2)) *| (ct|-|c)
+            ,testProperty "pz" $ pz =~= (i*sqrt(om_z/2)) *| (ct|-|c)
             -- @nonl
             -- @-node:gcross.20091207130316.1294:p_z
             -- @-others
@@ -205,11 +208,13 @@ main = defaultMain
         -- @    @+others
         -- @+node:gcross.20091206113753.1265:Rewritten with ladder operators
         [testProperty "Rewritten with ladder operators" $
-            original_hamiltonian ~=~ ladder_hamiltonian
+            original_hamiltonian =~= ladder_hamiltonian
+        -- @nonl
         -- @-node:gcross.20091206113753.1265:Rewritten with ladder operators
         -- @+node:gcross.20091206113753.1496:Diagonalized
         ,testProperty "Diagonalized" $
-            original_hamiltonian ~=~ diagonalized_hamiltonian
+            original_hamiltonian =~= diagonalized_hamiltonian
+        -- @nonl
         -- @-node:gcross.20091206113753.1496:Diagonalized
         -- @-others
         ]
@@ -217,10 +222,11 @@ main = defaultMain
     -- @+node:gcross.20091206205300.1499:Angular momentum tests
     ,testGroup "Angular momentum tests"
         -- @    @+others
-        -- @+node:gcross.20091206205300.1500:j_z
-        [testProperty "j_z" $
-            j_z ~=~ nt.n |-| gt.g
-        -- @-node:gcross.20091206205300.1500:j_z
+        -- @+node:gcross.20091206205300.1500:jz
+        [testProperty "jz" $
+            jz =~= nt.n |-| gt.g
+        -- @nonl
+        -- @-node:gcross.20091206205300.1500:jz
         -- @-others
         ]
     -- @-node:gcross.20091206205300.1499:Angular momentum tests
@@ -229,12 +235,14 @@ main = defaultMain
         -- @    @+others
         -- @+node:gcross.20091207130316.1276:#0
         [testProperty "#0" $
-            \f v -> (x.px.py.y ~=~ x.px.(y.py |-| i*|id)) f v &&
-                    (x.px.py.y ~=~ x.px.y.py |+| ((-i)*|x.px)) f v
+            \f -> (x.px.py.y =~= x.px.(y.py |-| i*|id)) f &&
+                  (x.px.py.y =~= x.px.y.py |+| ((-i)*|x.px)) f
+        -- @nonl
         -- @-node:gcross.20091207130316.1276:#0
         -- @+node:gcross.20091207130316.1281:#1
         ,testProperty "#1" $
-            negosum [x.px.py.y,y.py.px.x] ~=~ (-2)*|x.px.y.py |+| i*|(x.px|+|y.py)
+            negosum [x.px.py.y,y.py.px.x] =~= (-2)*|x.px.y.py |+| i*|(x.px|+|y.py)
+        -- @nonl
         -- @-node:gcross.20091207130316.1281:#1
         -- @+node:gcross.20091207130316.1283:#2
         ,testProperty "#2" $
@@ -242,12 +250,13 @@ main = defaultMain
              [z.pz.(px.x|+|py.y)
              ,pz.z.(x.px|+|y.py)
              ]
-            ~=~
+            =~=
             osum
              [(2*i)*|z.pz
              ,(-2)*|z.pz.(x.px|+|y.py)
              ,i*|(x.px|+|y.py)
              ]
+        -- @nonl
         -- @-node:gcross.20091207130316.1283:#2
         -- @+node:gcross.20091207130316.1279:#3
         ,testProperty "#3" $
@@ -259,12 +268,13 @@ main = defaultMain
              ,x.px.py.y
              ,y.py.px.x
              ]
-            ~=~
+            =~=
             osum
              [(2*i)*|(x.px|+|y.py|+|z.pz)
              ,(-2)*|z.pz.(x.px|+|y.py)
              ,(-2)*|x.px.y.py
              ]
+        -- @nonl
         -- @-node:gcross.20091207130316.1279:#3
         -- @+node:gcross.20091207130316.1286:#4
         ,testProperty "#4" $
@@ -273,7 +283,7 @@ main = defaultMain
              ,jy|^2
              ,jz|^2
              ]
-            ~=~
+            =~=
             osum
              [(z|^2).(px|^2 |+| py|^2)
              ,(pz|^2).(x|^2 |+| y|^2)
@@ -283,14 +293,17 @@ main = defaultMain
              ,(-2)*|z.pz.(x.px|+|y.py)
              ,(-2)*|x.px.y.py
              ]
+        -- @nonl
         -- @-node:gcross.20091207130316.1286:#4
         -- @+node:gcross.20091207130316.1288:#5
         ,testProperty "#5" $
-            x|^2 |+| y|^2 ~=~ (1/(2*om))*|t1 |+| (i/om)*|t2
+            x|^2 |+| y|^2 =~= (1/(2*om))*|t1 |+| (i/om)*|t2
+        -- @nonl
         -- @-node:gcross.20091207130316.1288:#5
         -- @+node:gcross.20091207130316.1290:#6
         ,testProperty "#6" $
-            px|^2 |+| py|^2 ~=~ (om/2)*|t1 |-| (i*om)*|t2
+            px|^2 |+| py|^2 =~= (om/2)*|t1 |-| (i*om)*|t2
+        -- @nonl
         -- @-node:gcross.20091207130316.1290:#6
         -- @-others
         ]

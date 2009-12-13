f326c7644c56acfe763ee3b9a99ccf877a3f50b0
Gregory Crosswhite <gcross@phys.washington.edu>
2009-12-12 16:21:21 -0800

    Modified code to use the simpler numeric operators.

--- test-fock.hs
+++ test-fock.hs
@@ -44,74 +44,80 @@ type QuantumOperator = DifferentialOperator (Complex Double)
 -- @-node:gcross.20091209132934.1288:QuantumOperator
 -- @-node:gcross.20091209132934.1287:Types
 -- @+node:gcross.20091204093401.3559:Operators
--- @+node:gcross.20091209132934.1289:x, y, x
+-- @+node:gcross.20091209132934.1289:x, y, z
 x, y, z :: QuantumOperator
 x = r_ X
 y = r_ Y
 z = r_ Z
--- @-node:gcross.20091209132934.1289:x, y, x
--- @+node:gcross.20091209132934.1290:px, py, px
+-- @-node:gcross.20091209132934.1289:x, y, z
+-- @+node:gcross.20091209132934.1290:px, py, pz
 px, py, pz :: QuantumOperator
 px = p_ X
 py = p_ Y
 pz = p_ Z
--- @-node:gcross.20091209132934.1290:px, py, px
+-- @-node:gcross.20091209132934.1290:px, py, pz
 -- @+node:gcross.20091204093401.3592:a, b, c
-a  = sqrt(om/2)  *|x |+| i*sqrt(1/(2*om))  *|px
-at = sqrt(om/2)  *|x |-| i*sqrt(1/(2*om))  *|px
-b  = sqrt(om/2)  *|y |+| i*sqrt(1/(2*om))  *|py
-bt = sqrt(om/2)  *|y |-| i*sqrt(1/(2*om))  *|py
-c  = sqrt(om_z/2)*|z |+| i*sqrt(1/(2*om_z))*|pz
-ct = sqrt(om_z/2)*|z |-| i*sqrt(1/(2*om_z))*|pz
+a  = sqrt(om/2)  *|x + i*sqrt(1/(2*om))  *|px
+at = sqrt(om/2)  *|x - i*sqrt(1/(2*om))  *|px
+b  = sqrt(om/2)  *|y + i*sqrt(1/(2*om))  *|py
+bt = sqrt(om/2)  *|y - i*sqrt(1/(2*om))  *|py
+c  = sqrt(om_z/2)*|z + i*sqrt(1/(2*om_z))*|pz
+ct = sqrt(om_z/2)*|z - i*sqrt(1/(2*om_z))*|pz
 -- @-node:gcross.20091204093401.3592:a, b, c
 -- @+node:gcross.20091206113753.1374:g, n
-g  = 1/sqrt 2*|(a  |+| i*|b )
-gt = 1/sqrt 2*|(at |-| i*|bt)
-n  = 1/sqrt 2*|(b  |+| i*|a )
-nt = 1/sqrt 2*|(bt |-| i*|at)
+g  = 1/sqrt 2*|(a  + i*|b )
+gt = 1/sqrt 2*|(at - i*|bt)
+n  = 1/sqrt 2*|(b  + i*|a )
+nt = 1/sqrt 2*|(bt - i*|at)
 -- @-node:gcross.20091206113753.1374:g, n
 -- @+node:gcross.20091206205300.1498:jx, jy, jz
-jx = y.pz |-| z.py
-jy = z.px |-| x.pz
-jz = x.py |-| y.px
+jx = y.pz - z.py
+jy = z.px - x.pz
+jz = x.py - y.px
 -- @-node:gcross.20091206205300.1498:jx, jy, jz
 -- @-node:gcross.20091204093401.3559:Operators
 -- @+node:gcross.20091204093401.3557:Hamiltonians
 -- @+node:gcross.20091204093401.3558:Original
 original_hamiltonian :: QuantumOperator
 original_hamiltonian =
-     1/2 *| (px|^2 |+| py|^2 |+| pz|^2)
- |+| bigOmega *| ( x.py |-| y.px )
- |+| om**2/2 *| ( x|^2 |+| y|^2 )
- |+| om_z**2/2 *| z|^2
+     1/2 *| (px|^2 + py|^2 + pz|^2)
+ + bigOmega *| ( x.py - y.px )
+ + om**2/2 *| ( x|^2 + y|^2 )
+ + om_z**2/2 *| z|^2
 -- @-node:gcross.20091204093401.3558:Original
 -- @+node:gcross.20091204093401.3595:Ladder
 ladder_hamiltonian :: QuantumOperator
 ladder_hamiltonian =
-     om *| (at.a |+| bt.b |+| id)
- |+| om_z *| (ct.c |+| 1/2*|id)
- |-| i*bigOmega *| (at.b |-| bt.a)
+     om *| (at.a + bt.b + id)
+ + om_z *| (ct.c + 1/2*|id)
+ - i*bigOmega *| (at.b - bt.a)
 -- @-node:gcross.20091204093401.3595:Ladder
 -- @+node:gcross.20091206113753.1494:Diagonalized
 diagonalized_hamiltonian :: QuantumOperator
 diagonalized_hamiltonian =
-     om *| (gt.g |+| nt.n |+| id)
- |+| om_z *| (ct.c |+| 1/2*|id)
- |-| bigOmega *| (gt.g |-| nt.n)
+     om *| (gt.g + nt.n + id)
+ + om_z *| (ct.c + 1/2*|id)
+ - bigOmega *| (gt.g - nt.n)
 -- @-node:gcross.20091206113753.1494:Diagonalized
 -- @-node:gcross.20091204093401.3557:Hamiltonians
 -- @+node:gcross.20091204093401.3991:Functions
+-- @+node:gcross.20091212141130.1615:vx, vy, vz
+vx, vy, vz :: (Complex Double,Complex Double,Complex Double) -> FunctionExpansion (Complex Double)
+vx = v_ X
+vy = v_ Y
+vz = v_ Z
+-- @-node:gcross.20091212141130.1615:vx, vy, vz
 -- @+node:gcross.20091204093401.3992:makeHOFromCoordinates
-makeHOFromCoordinates q p om = 1/2*|p|^2 |+| 1/2*om**2*|q|^2
+makeHOFromCoordinates q p om = 1/2*|p|^2 + 1/2*om**2*|q|^2
 -- @-node:gcross.20091204093401.3992:makeHOFromCoordinates
 -- @+node:gcross.20091204093401.3994:makeHOFromLadders
-makeHOFromLadders a at om = om*|(at.a |+| 0.5*|id)
+makeHOFromLadders a at om = om*|(at.a + 0.5*|id)
 -- @-node:gcross.20091204093401.3994:makeHOFromLadders
 -- @-node:gcross.20091204093401.3991:Functions
 -- @+node:gcross.20091207130316.1295:Miscellaneous expressions
 -- @+node:gcross.20091207130316.1296:t1/t2
 t1 = osum [gt.g,g.gt,nt.n,n.nt]
-t2 = nt.gt |-| n.g
+t2 = nt.gt - n.g
 -- @-node:gcross.20091207130316.1296:t1/t2
 -- @-node:gcross.20091207130316.1295:Miscellaneous expressions
 -- @-others
@@ -174,32 +180,40 @@ main = defaultMain
         ,testGroup "Correct definition of old in terms of new"
             -- @    @+others
             -- @+node:gcross.20091206205300.1384:x
-            [testProperty "x" $ x =~= (1/2/sqrt(om)) *| (gt|+|g |+| i*|(nt|-|n) )
-            -- @nonl
+            [testProperty "x" $ x =~= (1/2/sqrt(om)) *| (gt+g + i*|(nt-n))
             -- @-node:gcross.20091206205300.1384:x
             -- @+node:gcross.20091206205300.1493:y
-            ,testProperty "y" $ y =~= (1/2/sqrt(om)) *| (nt|+|n |+| i*|(gt|-|g) )
+            ,testProperty "y" $ y =~= (1/2/sqrt(om)) *| (nt+n + i*|(gt-g) )
             -- @nonl
             -- @-node:gcross.20091206205300.1493:y
             -- @+node:gcross.20091207130316.1292:z
-            ,testProperty "z" $ z =~= (1/sqrt(2*om_z)) *| (ct|+|c)
+            ,testProperty "z" $ z =~= (1/sqrt(2*om_z)) *| (ct+c)
             -- @nonl
             -- @-node:gcross.20091207130316.1292:z
             -- @+node:gcross.20091206205300.1495:p_x
-            ,testProperty "px" $ px =~= (i*sqrt(om)/2) *| (gt|-|g |+| i*|(nt|+|n) )
+            ,testProperty "px" $ px =~= (i*sqrt(om)/2) *| (gt-g + i*|(nt+n) )
             -- @nonl
             -- @-node:gcross.20091206205300.1495:p_x
             -- @+node:gcross.20091206205300.1497:p_y
-            ,testProperty "py" $ py =~= (i*sqrt(om)/2) *| (nt|-|n |+| i*|(gt|+|g) )
+            ,testProperty "py" $ py =~= (i*sqrt(om)/2) *| (nt-n + i*|(gt+g) )
             -- @nonl
             -- @-node:gcross.20091206205300.1497:p_y
             -- @+node:gcross.20091207130316.1294:p_z
-            ,testProperty "pz" $ pz =~= (i*sqrt(om_z/2)) *| (ct|-|c)
+            ,testProperty "pz" $ pz =~= (i*sqrt(om_z/2)) *| (ct-c)
             -- @nonl
             -- @-node:gcross.20091207130316.1294:p_z
             -- @-others
             ]
         -- @-node:gcross.20091206205300.1382:Correct definition of old in terms of new
+        -- @+node:gcross.20091212141130.1304:Correct simplification of the new
+        ,testGroup "Correct simplification of the new"
+            -- @    @+others
+            -- @+node:gcross.20091212141130.1305:n
+            [testProperty "n" $ n =~= (sqrt(om)/2) *| (i*|x - (1/om)*|px + y + (i/om)*|py)
+            -- @-node:gcross.20091212141130.1305:n
+            -- @-others
+            ]
+        -- @-node:gcross.20091212141130.1304:Correct simplification of the new
         -- @-others
         ]
     -- @-node:gcross.20091204093401.3987:Ladder operator tests
@@ -224,8 +238,7 @@ main = defaultMain
         -- @    @+others
         -- @+node:gcross.20091206205300.1500:jz
         [testProperty "jz" $
-            jz =~= nt.n |-| gt.g
-        -- @nonl
+            jz =~= nt.n - gt.g
         -- @-node:gcross.20091206205300.1500:jz
         -- @-others
         ]
@@ -235,26 +248,26 @@ main = defaultMain
         -- @    @+others
         -- @+node:gcross.20091207130316.1276:#0
         [testProperty "#0" $
-            \f -> (x.px.py.y =~= x.px.(y.py |-| i*|id)) f &&
-                  (x.px.py.y =~= x.px.y.py |+| ((-i)*|x.px)) f
+            \f -> (x.px.py.y =~= x.px.(y.py - i*|id)) f &&
+                  (x.px.py.y =~= x.px.y.py + ((-i)*|x.px)) f
         -- @nonl
         -- @-node:gcross.20091207130316.1276:#0
         -- @+node:gcross.20091207130316.1281:#1
         ,testProperty "#1" $
-            negosum [x.px.py.y,y.py.px.x] =~= (-2)*|x.px.y.py |+| i*|(x.px|+|y.py)
+            negosum [x.px.py.y, y.py.px.x] =~= (-2)*|x.px.y.py + i*|(x.px+y.py)
         -- @nonl
         -- @-node:gcross.20091207130316.1281:#1
         -- @+node:gcross.20091207130316.1283:#2
         ,testProperty "#2" $
             negosum
-             [z.pz.(px.x|+|py.y)
-             ,pz.z.(x.px|+|y.py)
+             [z.pz.(px.x+py.y)
+             ,pz.z.(x.px+y.py)
              ]
             =~=
             osum
              [(2*i)*|z.pz
-             ,(-2)*|z.pz.(x.px|+|y.py)
-             ,i*|(x.px|+|y.py)
+             ,(-2)*|z.pz.(x.px + y.py)
+             ,i*|(x.px+y.py)
              ]
         -- @nonl
         -- @-node:gcross.20091207130316.1283:#2
@@ -270,8 +283,8 @@ main = defaultMain
              ]
             =~=
             osum
-             [(2*i)*|(x.px|+|y.py|+|z.pz)
-             ,(-2)*|z.pz.(x.px|+|y.py)
+             [(2*i)*|(x.px + y.py + z.pz)
+             ,(-2)*|z.pz.(x.px + y.py)
              ,(-2)*|x.px.y.py
              ]
         -- @nonl
@@ -285,25 +298,23 @@ main = defaultMain
              ]
             =~=
             osum
-             [(z|^2).(px|^2 |+| py|^2)
-             ,(pz|^2).(x|^2 |+| y|^2)
+             [(z|^2).(px|^2 + py|^2)
+             ,(pz|^2).(x|^2 + y|^2)
              ,(x|^2).(py|^2)
              ,(px|^2).(y|^2)
-             ,(2*i)*|(x.px|+|y.py|+|z.pz)
-             ,(-2)*|z.pz.(x.px|+|y.py)
+             ,(2*i)*|(x.px + y.py + z.pz)
+             ,(-2)*|z.pz.(x.px + y.py)
              ,(-2)*|x.px.y.py
              ]
         -- @nonl
         -- @-node:gcross.20091207130316.1286:#4
         -- @+node:gcross.20091207130316.1288:#5
         ,testProperty "#5" $
-            x|^2 |+| y|^2 =~= (1/(2*om))*|t1 |+| (i/om)*|t2
-        -- @nonl
+            x|^2 + y|^2 =~= (1/(2*om))*|t1 + (i/om)*|t2
         -- @-node:gcross.20091207130316.1288:#5
         -- @+node:gcross.20091207130316.1290:#6
         ,testProperty "#6" $
-            px|^2 |+| py|^2 =~= (om/2)*|t1 |-| (i*om)*|t2
-        -- @nonl
+            px|^2 + py|^2 =~= (om/2)*|t1 - (i*om)*|t2
         -- @-node:gcross.20091207130316.1290:#6
         -- @-others
         ]
